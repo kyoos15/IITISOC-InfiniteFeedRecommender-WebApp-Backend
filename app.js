3f8106e1174eafaa4a97854de3289fda15830235
Kaal-09 <sakshamgautam369@gmail.com>
2025-06-04 12:20:24 +0530

    utils done routers start

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import userRoutes from './src/routes/user.routes.js'
+import assetRoutes from './src/routes/user.routes.js'
+
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }))
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true, 
+    })
+);
 
 app.get('/', (req, res)=> {
     res.send('Ishaan is gay')
 })
 
-app.listen(4000);
+app.use('/api/user', userRoutes);
+app.use('/api/asset', assetRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
+})
