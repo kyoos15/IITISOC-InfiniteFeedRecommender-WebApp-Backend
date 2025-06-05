a1ce10a2a6e29b067fa301ab85ba6831ef068dd7
Kushagra  Shrikhande <158499628+kyoos15@users.noreply.github.com>
2025-06-05 23:30:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import connectDB from './src/utils/utils.js';
-import userRoutes from './src/routes/user.routes.js'
-import assetRoutes from './src/routes/user.routes.js'
 
-dotenv.config();
 
+const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }))
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true, 
-    })
-);
-
-app.get('/', (req, res)=> {
-    res.send('Ishaan is gay')
-})
-
-app.use('/api/user', userRoutes);
-app.use('/api/asset', assetRoutes);
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-    connectDB();
-})
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
