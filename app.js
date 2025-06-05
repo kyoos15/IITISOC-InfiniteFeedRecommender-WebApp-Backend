279d7a060c513693b80e75726373f9c7a55cd085
Kushagra  Shrikhande <158499628+kyoos15@users.noreply.github.com>
2025-06-05 11:40:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ import userRoutes from './src/routes/user.routes.js'
 import assetRoutes from './src/routes/user.routes.js'
 
 dotenv.config();
-
+const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
+
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 app.use(
@@ -27,7 +30,8 @@ app.get('/', (req, res)=> {
 app.use('/api/user', userRoutes);
 app.use('/api/asset', assetRoutes);
 
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     connectDB();
